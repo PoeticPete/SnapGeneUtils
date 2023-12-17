a1c4afdfffaca2b4aeff40eb9e9478d7be64ab22
Peter Tao <petertao95@gmail.com>
2023-12-17 00:59:48 -0500

    Fix build

--- snapgene_utils/__init__.py
+++ snapgene_utils/__init__.py
@@ -1,10 +1,10 @@
 """
-snapgene_utils module
+snapgene_reader module
 usage:
     from snapgene_utils import snapgene_file_to_dict
     obj = snapgene_file_to_dict(file_path='test.dna')
 """
-from .snapgene_utils import (
+from .snapgene_reader import (
     snapgene_file_to_dict,
     snapgene_file_to_gbk,
     snapgene_file_to_seqrecord,
